85e85502185888efb2170033ec62f6a7d86a1fc8
zinx | Tasdid <tasdid110@gmail.com>
2023-03-03 23:52:22 +0600

    Create Icon.tsx

---
+++ Icon.tsx
@@ -0,0 +1,17 @@
+// basic icon containing an image. Change the size according to your need, or make it dynamic. 
+
+import Image from "next/image";
+
+interface iconProp {
+  img: string;
+}
+
+const Icon = ({ img }: iconProp) => {
+  return (
+    <div className="w-[32px] h-[32px] relative flex justify-center items-center">
+      <Image alt="." src={img} fill className="object-cover" />
+    </div>
+  );
+};
+
+export default Icon;
